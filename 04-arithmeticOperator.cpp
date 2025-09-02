b043a6125b6e8ba1c2a43b1885c8001f6ba00aa1
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-02 07:06:42 +0530

    Learning Arithmetic Operator

---
+++ 04-arithmeticOperator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int num1, num2; //declaration
+  num1 = 5; //initialization
+  num2 = 6; //initialization
+
+  cout << num1 + num2 << endl; // addition operator - sum
+  cout << num1 - num2 << endl; // subctraction operator - difference
+  cout << num1 * num2 << endl; // multiplication operator - multiply
+  cout << num1 / num2 << endl; // division operator - gives quotient as output
+  cout << num1 % num2 << endl; // modulo operator - gives remainder as output
+
+
+  // Increment & Decrement Operator 
+  num1++; // post increment operator - first use then increase
+  cout << num1 << endl;
+  num1--; // post decrement operator - first use then decrease
+  cout << num1 << endl;
+  ++num1; // pre increment operator - first increase then use
+  cout << num1 << endl;
+  --num1; // pre derement operator - first decrease then use
+  cout << num1;
+}
