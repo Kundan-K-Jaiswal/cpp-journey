52fd69bb9a599a84cdf24d5d52334f18cd0a7606
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-02 06:23:46 +0530

    Learning About Comments

---
+++ 03-comments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+  // Comments are something that is ignored by compiler but is helpful for the programmers to understand the code
+  
+  // this is single line comment.
+  
+  /*
+  And
+  this 
+  is 
+  multi-line
+  comment
+  */ 
+
+  return 0;
+}
