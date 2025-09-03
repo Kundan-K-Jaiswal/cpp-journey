170e32121f942d5059d94b0899ba9ecfd31d6a6a
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-03 05:51:03 +0530

    Practicing Questions

---
+++ question-01-areaCircle.cpp
@@ -0,0 +1,13 @@
+// Question : Write a program to calculate area of circle.
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  float radius, area;
+  radius = 3.6;
+  area = 3.14 * radius * radius;
+  cout << "The Area Of Given Circle Is " << area;
+  return 0;
+}
