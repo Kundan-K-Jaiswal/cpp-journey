343b50035b7f622e13aaadc8a51039898b35c744
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-13 05:33:53 +0530

    Understanding For Loop

---
+++ 12-forLoop.cpp
@@ -0,0 +1,18 @@
+/*
+
+for(initilization; condition; updation){
+codes that is to be executed multiple times
+}
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  //for loop syntax
+  for (int i = 0; i<10; i++ ) {
+    cout << i+1 << ". Hello World!" << endl;
+  }
+  return 0;
+}
