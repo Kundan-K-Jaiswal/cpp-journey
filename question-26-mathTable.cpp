6238217bea370531bd719ac054e2aa733b5536a6
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-13 05:36:49 +0530

    Printing table of 19

---
+++ question-26-mathTable.cpp
@@ -0,0 +1,13 @@
+// Print the table of 19
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  //for loop
+  for (int i=1; i<=10; i++) {
+    cout << "19 x " << i << " = " << 19 * i << endl;
+  }
+  
+  return 0;
+}
