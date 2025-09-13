dc8cfe8e62a297fe3e9988692e757a878c8b781d
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-13 05:36:13 +0530

    Printing all even numbers from 1 to 100 using for loop.

---
+++ question-25-loopEvenNum.cpp
@@ -0,0 +1,12 @@
+// Print all the even numbers form 1 to 100
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  //for loop
+  for (int i = 2; i<=100; i+=2) {
+    cout << i << " ";
+  }
+  return 0;
+}
