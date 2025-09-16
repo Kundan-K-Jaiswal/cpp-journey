bb87c8692f480af21e15ef3f47c4c485bdebce79
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-17 05:21:27 +0530

    Printing Right Star Triangle Pattern

---
+++ question-55-starTriangleR.cpp
@@ -0,0 +1,27 @@
+/*
+Print The Right Star Triangle Pattern Using Nested Loops where "n = no of rows"
+eg : 
+n = 5
+# # # # *
+# # # * *
+# # * * *
+# * * * *
+* * * * *
+*/
+
+#include<iostream>
+using namespace std;
+int main () {
+  int n;
+  cin >> n;
+  for (int i = 1; i<=n; i++){
+    for(int j = n-1; j>=i; j--){
+      cout << "  ";
+    }
+    for(int k=1; k<=i; k++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
