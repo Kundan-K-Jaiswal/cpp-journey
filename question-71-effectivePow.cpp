0e6d93bdd7041ae6823e2c9398db1575b53c9910
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-09-27 12:44:48 +0530

    Creating a effective power function

---
+++ question-71-effectivePow.cpp
@@ -0,0 +1,19 @@
+// Write a pow function using recurstion
+
+#include<iostream>
+using namespace std;
+
+int power(int a, int b) {
+  if (b == 1) return a;
+  if ( b % 2 == 0) {
+    return power(a,b/2) * power(a,b/2);
+  }
+  return a * power(a, b-1);
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << power(a,b);
+  return 0;
+}
