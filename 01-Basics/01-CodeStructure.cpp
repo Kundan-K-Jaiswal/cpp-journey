75bb2dcb7c6a9ab56f6c41023920d3de522dc5ae
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-03-08 08:33:46 +0530

    Boilerplate Code in C++

--- 01-Basics/01-CodeStructure.cpp
+++ 01-Basics/01-CodeStructure.cpp
@@ -1,4 +1,9 @@
-#include<iostream> // pre processor - iostream is used for using cout and cin
-int main() { // Main function from where the code execution starts
+#include<iostream>                // pre processor - iostream (header file) is used for using cout and cin
+
+int main() {                     // main function from where the code execution starts
+
   std :: cout << "Hello World"; // cout is used to print text/data to the console (screen)
-  return 0; // Returning 0 indicates successful termination to the operating system.
+
+  return 0;                    // returning 0 indicates successful termination to the operating system.
+
+}
