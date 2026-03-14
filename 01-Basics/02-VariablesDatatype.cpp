03e3caaa1d95185819827f67204511c14071d397
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-03-14 05:54:19 +0530

    Understanding Datatypes

--- 01-Basics/02-VariablesDatatype.cpp
+++ 01-Basics/02-VariablesDatatype.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-using namespace std; // used so that we dont have to write std:: infornt of cout and cin.
+using namespace std;                                      // used so that we dont have to write std:: infornt of cout and cin.
 int main() {
-  int integer = 5; // used to store integers
-  float decimalNumber = 5.001; // used to store decimal numbers
-  double preciseDecimalNumber = 5.666666666666986532; // used to store decimal numbers with precision
-  char character = 'k'; // used to store characters
-  bool state = true; // used to store either ture or false
-  cout << integer << " " << decimalNumber << " " << character << " " << state << " " << preciseDecimalNumber;
+  int integer = 5;                                      // used to store integers
+  float decimalNumber = 5.001f;                        // used to store decimal numbers
+  double preciseDecimalNumber = 5.66698329;           // used to store decimal numbers with precision ( precision of cout is only 5 characters after ".")
+  char character = 'k';                              // used to store characters ( is stored in ASCII Value )
+  bool isState = true;                              // used to store either ture or false
+  cout << integer << " " << decimalNumber << " " << character << " " << isState << " " << preciseDecimalNumber;
+  return 0;
 }
