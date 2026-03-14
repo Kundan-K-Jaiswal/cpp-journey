ae3c9528b7d2c1c8117023eb4786c6e580563454
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-03-14 05:56:56 +0530

    Understanding Comments

--- 01-Basics/03-Comments.cpp
+++ 01-Basics/03-Comments.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 int main() {
   // Comments are something that is ignored by compiler but is helpful for the programmers to understand the code
+
+  // There are 2 types of comments :
+  // 1. Single Line Comment
+  // 2. Multi-Line Comment
   
-  // this is single line comment.
+  // This is single line comment.
   
   /*
   And
