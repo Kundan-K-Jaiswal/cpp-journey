cf7ebcb5a11de2d681bece0d5a383d1bc5690f81
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-03-27 06:52:43 +0530

    Solving Problem No.1 On LeetCode

---
+++ 03-DSA/Arrays/04-TwoSum-LC1.cpp
@@ -0,0 +1,58 @@
+// 1. Two Sum | Platform : LeetCode
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+// Constraints:
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> twoSum(vector<int> nums, int target) {
+  for (int i = 0; i<nums.size(); i++ ) {
+    for ( int j = i+1; j<nums.size(); j++ ) {
+        if (nums[i] + nums[j] == target ) {
+          return {i,j};
+        }
+    }
+  }
+}
+
+int main() {
+  vector<int> nums = {2,7,11,15};
+  int target = 9;
+
+  vector<int> result = twoSum(nums, target);
+  for ( auto val: result ) {
+    cout << val << " ";
+  }
+
+  return 0;
+}
+
+
+
+// Approach : Brute Force
+// Concept : Checking each combination to find the pair whose sum is equal to target value 
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
