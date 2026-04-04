bcec96d7ed1a16b5e9c855fb5e63da48571f3673
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-04-04 06:24:34 +0530

    Solving Problem No.4 On LeetCode

---
+++ 03-DSA/Arrays/05-MedianOfTwoSortedArrays-LC4.cpp
@@ -0,0 +1,56 @@
+// 4. Median of Two Sorted Arrays | Platform : LeetCode 
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// The overall run time complexity should be O(log (m+n)).
+
+// Example 1:
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+
+// Example 2:
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+// Constraints:
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for(int val: nums2) {
+            nums1.push_back(val);
+        }
+        sort(nums1.begin(), nums1.end());
+
+        int size = nums1.size();
+        float ans = nums1[(size/2)];
+        if ( size % 2 == 0 ) {
+            ans += nums1[(size/2)-1];
+            ans /= 2;
+        }
+        return ans;
+}
+
+int main() {
+    vector<int> nums1 = {1}, nums2 ={2};
+    cout << findMedianSortedArrays(nums1,nums2);
+    return 0;
+}
+
+
+
+// Approach : Merge & Sort
+// Concept : Merge & sort the array, then find number of elements, if odd return middle element else return mean of middle elements.
+// Time Complexity : O((m+n)log(m+n))
+// Space Complexity : O(m+n)
