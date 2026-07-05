e159d5c9ea1ca4185f88f4dad0cd7d59d36ca670
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-07-05 19:08:14 +0530

    Solving Problems on LeetCode

---
+++ 03-DSA/01-Array/102-MoveZeroes-LC283.cpp
@@ -0,0 +1,46 @@
+
+// 283. Move Zeroes    |  Platform : LeetCode
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of 
+// the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [0]
+ 
+// Constraints:
+// 1 <= nums.length <= 10^4
+// -2^31 <= nums[i] <= 2^31 - 1
+
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+        for (int val : nums ) {
+            if ( val != 0 ) {
+                nums[pos++] = val;
+            }
+        }
+        for ( pos; pos<nums.size(); pos++) {
+            nums[pos] = 0;
+        }
+    }
+
+int main() {
+    vector<int> nums = {0,1,0,3,12};
+    moveZeroes(nums);
+    for ( int val : nums ) {
+        cout << val << " ";
+    }
+    return 0;
+}
