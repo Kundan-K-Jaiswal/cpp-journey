7e42f017bac0f2b4fc2a114c63e980f812210e95
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-07-18 06:39:49 +0530

    Solving Problems On LeetCode

---
+++ 03-DSA/02-String/2942-FindWordsContainingCharacter-LC.cpp
@@ -0,0 +1,63 @@
+
+// 2942. Find Words Containing Character  |  Platform : LeetCode
+
+// You are given a 0-indexed array of strings words and a character x.
+// Return an array of indices representing the words that contain the character x.
+// Note that the returned array may be in any order.
+
+ 
+// Example 1:
+// Input: words = ["leet","code"], x = "e"
+// Output: [0,1]
+// Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+// Example 2:
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+// Output: [0,2]
+// Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+// Example 3:
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+// Output: []
+// Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+// Constraints:
+// 1 <= words.length <= 50
+// 1 <= words[i].length <= 50
+// x is a lowercase English letter.
+// words[i] consists only of lowercase English letters.
+
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> findWordsContaining(vector<string>& words, char x) {
+    vector<int> ans;
+    int i = 0;
+
+    for ( string word : words ) {
+        for ( char ch : word ) {
+            if ( ch == x ) {
+                ans.push_back(i);
+                break;
+            }
+        }
+        i++;
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<string> words = {"abc","bcd","aaaa","cbc"};
+    char x = 'a';
+    vector<int> result =  findWordsContaining(words,x);
+    for ( int val : result ) {
+        cout << val << " ";
+    }
+    return 0;
+}
