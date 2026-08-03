6fa47966d3196d099f5eea8153242d1f1db453b0
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-08-03 05:31:19 +0530

    Solving Problems On LeetCode

---
+++ 03-DSA/01-Array/1464-MaximumProductOfTwoElementsInAnArray-LC.cpp
@@ -0,0 +1,57 @@
+
+// 1464. Maximum Product of Two Elements in an Array  |  Platform : LeetCode
+
+
+// Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value 
+// of (nums[i]-1)*(nums[j]-1).
+ 
+
+// Example 1:
+// Input: nums = [3,4,5,2]
+// Output: 12
+
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, 
+// (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+
+
+// Example 2:
+// Input: nums = [1,5,4,5]
+// Output: 16
+
+// Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+
+
+// Example 3:
+// Input: nums = [3,7]
+// Output: 12
+ 
+
+// Constraints:
+// 2 <= nums.length <= 500
+// 1 <= nums[i] <= 10^3
+
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+    int n1 = INT16_MIN, n2 = INT16_MIN;
+    for ( int val : nums ) {
+        if ( val > n1 ) {
+            n2 = n1;
+            n1 = val;
+        } else if ( val > n2 ) {
+            n2 = val;
+        }
+    }
+    return (n1-1)*(n2-1);
+}
+
+int main() {
+    vector<int> nums = {3,4,5,2};
+    cout << maxProduct(nums);
+    return 0;
+}
