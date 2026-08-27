a30e3f2fd118535780608fe11b3678e42fd36289
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2026-08-27 05:52:49 +0530

    Solving Problems On LeetCode

---
+++ 03-DSA/02-String/1507-ReformatDate-LC.cpp
@@ -0,0 +1,74 @@
+
+// 1507. Reformat Date  |  Platform : LeetCode
+
+
+// Given a date string in the form Day Month Year, where:
+// Day is in the set {"1st", "2nd", "3rd", "4th", ..., "30th", "31st"}.
+// Month is in the set {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}.
+// Year is in the range [1900, 2100].
+
+// Convert the date string to the format YYYY-MM-DD, where:
+// YYYY denotes the 4 digit year.
+// MM denotes the 2 digit month.
+// DD denotes the 2 digit day.
+ 
+
+// Example 1:
+// Input: date = "20th Oct 2052"
+// Output: "2052-10-20"
+
+
+// Example 2:
+// Input: date = "6th Jun 1933"
+// Output: "1933-06-06"
+
+
+// Example 3:
+// Input: date = "26th May 1960"
+// Output: "1960-05-26"
+ 
+
+// Constraints:
+// The given dates are guaranteed to be valid, so no error handling is necessary.
+
+
+
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+string reformatDate(string date) {
+    string day = "", month = "", year = "";
+    int i = 0, n = date.length();
+    while ( (char)isdigit(date[i]) ) {
+        day += date[i];
+        i++;
+    } 
+    if ( day.length() == 1 ) {
+        day += day;
+        day[0] = '0';
+    }
+
+    year += date[n-4];
+    year += date[n-3];
+    year += date[n-2];
+    year += date[n-1];
+
+    month += date[n-8];
+    month += date[n-7];
+    month += date[n-6];
+
+    unordered_map<string,string> mp = {{"Jan","01"},{"Feb","02"}, {"Mar","03"}, {"Apr","04"}, {"May","05"}, {"Jun","06"}, 
+                                        {"Jul","07"}, {"Aug","08"}, {"Sep","09"}, {"Oct","10"}, {"Nov","11"}, {"Dec","12"}};
+
+    month = mp[month];
+
+    return year + "-" + month + "-" + day;
+}
+
+int main() {
+    string date = "20th Oct 2052";
+    cout << reformatDate(date);
+    return 0;
+}
